b2ea114e66954265788a0f9a5de0d94ef8c9c978
mones <mones@Mones>
2014-05-28 15:21:44 +0100

    commit V21
    Signed-off-by: mones <mones@Mones>

---
+++ integration-demo2/src/main/java/edu/esprit/integration/demo/managed/beans/CalculatorsManagedBean.java
@@ -0,0 +1,14 @@
+package edu.esprit.integration.demo.managed.beans;
+
+public class CalculatorsManagedBean {
+	
+	
+	
+	public CalculatorsManagedBean(){
+		
+	}
+	
+	int a ;
+	int b;
+
+}
---
+++ integration-demo2/src/main/java/edu/esprit/integration/demo/services/CalculatorService.java
@@ -0,0 +1,30 @@
+package edu.esprit.integration.demo.services;
+
+
+
+public class CalculatorService {
+	
+	public CalculatorService(){
+		
+	}
+
+	
+	public int sommation (int a , int b){
+		return a+b;
+	}
+	
+	public int soustraction (int a , int b){
+		return a-b;
+	}
+	
+	
+	
+	public int produit (int a , int b){
+		return a*b;
+	}
+	
+	
+	public int division  (int a , int b){
+		return a/b;
+	}
+}
